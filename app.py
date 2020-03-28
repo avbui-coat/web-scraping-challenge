b561150e5a8072aa7fff038bb86b3eb8aec242d4
An Bui <hvbuianbui@gmail.com>
2020-03-28 02:35:26 -0700

    wrapped up

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mars_scraping
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -12,13 +12,22 @@ def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
+# @app.route("/mars_image")
+# def image():
+#     mars = mongo.db.mars.find_one()
+#     return render_template("index.html", mars=mars)
+
+# @app.route("/news")
+# def news():
+#     mars = mongo.db.mars.find_one()
+#     return render_template("index.html", mars=mars)
+
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = mars_scraping.scrape_all()
-    mars.replace_one({}, mars_data, upsert=True)
-    return "Scraping Successful!"
-
+    mars_data = mars_scraping.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug = True)
